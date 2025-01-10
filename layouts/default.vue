1e5440aa15c7caf8cb084971a43225ac18d57489
Rafael <rafamed.tech@gmail.com>
2025-01-10 11:54:40 -0800

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="p-4 bg-gradient-to-br from-blue-50 to-green-50 min-h-screen z-0">
+    <header class="flex justify-between items-center h-24 z-20">
+      <section class="h-full">
+        <NuxtLink to="/">
+          <img src="@/assets/images/logo.jpeg" alt="" class="w-full h-full" />
+        </NuxtLink>
+      </section>
+      <nav class="flex md:space-x-4 items-center">
+        <ul class="space-x-12 hidden md:flex">
+          <li>
+            <NuxtLink to="/">Inicio</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/">Servicios</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/">Certificaciones</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/">Sectores atendidos</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/">Contacto</NuxtLink>
+          </li>
+        </ul>
+        <section class="flex items-center space-x-2">
+          <UButton to="/" color="green">Agenda una cita</UButton>
+          <UButton to="/" color="green" icon="i-heroicons-bars-3-bottom-right" variant="outline" class="md:hidden" />
+        </section>
+      </nav>
+    </header>
+    <main class="relative">
+      <slot />
+    </main>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts"></script>
+
+<template>
+  <UContainer>
+    <section class="flex gap-8 md:gap-16 flex-col md:flex-row justify-between items-center min-h-[70vh] py-12">
+      <section class="flex flex-col gap-4 md:gap-8 md:basis-3/5">
+        <h1 class="text-4xl md:text-6xl font-semibold text-primary leading-tight">
+          Comprometidos con la sanidad vegetal y la seguridad agroalimentaria
+        </h1>
+        <p class="text-lg md:text-xl">
+          Brindamos servicios de diagnóstico confiables para el manejo eficiente de plagas y la calidad agroalimentaria.
+        </p>
+        <section class="flex gap-4">
+          <UButton to="/" color="green" variant="outline" size="lg" icon="i-heroicons-arrow-small-right"
+            >Nuestros servicios</UButton
+          >
+          <UButton to="/" color="green" size="lg" icon="i-heroicons-phone-arrow-down-left">Contáctanos</UButton>
+        </section>
+      </section>
+      <figure class="basis-2/5">
+        <img src="@/assets/images/valores.png" alt="" class="w-full" />
+      </figure>
+    </section>
+
+    <UDivider />
+
+    <section class="py-12 flex justify-between gap-8 items-center">
+      <figure class="w-1/3 h-full rounded-xl overflow-hidden">
+        <img src="@/assets/images/hero.jpg" alt="" />
+      </figure>
+      <div class="w-1/2">
+        <h2 class="text-5xl font-semibold text-primary">Sobre CISEF</h2>
+        <p class="text-xl mt-4">
+          Desde 1997, somos un laboratorio certificado por SADER que contribuye al manejo sostenible de plagas agrícolas
+          y a la calidad agroalimentaria.
+        </p>
+      </div>
+    </section>
+
+    <UDivider />
+
+    <section class="py-12">
+      <h2 class="text-5xl font-semibold text-primary text-center">Nuestros servicios</h2>
+      <p class="text-xl text-center mt-4">Diagnósticos especializados para la sanidad y calidad agroalimentaria.</p>
+      <section class="grid grid-cols-1 md:grid-cols-4 gap-8 mt-8">
+        <UCard class="relative overflow-hidden h-96 grid place-content-center p-2">
+          <h3 class="text-3xl font-semibold text-gray-100 z-10 relative">Fitosanidad</h3>
+          <img
+            src="@/assets/images/fitosanidad.jpg"
+            alt=""
+            class="absolute inset-0 z-0 brightness-50 h-full object-cover w-full"
+          />
+        </UCard>
+        <UCard class="relative overflow-hidden h-96 grid place-content-center p-2">
+          <h3 class="text-3xl font-semibold text-gray-100 z-10 relative">Inocuidad Agroalimentaria</h3>
+          <img
+            src="@/assets/images/inocuidad.jpg"
+            alt=""
+            class="absolute inset-0 z-0 brightness-50 h-full object-cover w-full"
+          />
+        </UCard>
+        <UCard class="relative overflow-hidden h-96 grid place-content-center p-2">
+          <h3 class="text-3xl font-semibold text-gray-100 z-10 relative">Sanidad Acuícola y Pesquera</h3>
+          <img
+            src="@/assets/images/acuicola.png"
+            alt=""
+            class="absolute inset-0 z-0 brightness-50 h-full object-cover w-full"
+          />
+        </UCard>
+        <UCard class="relative overflow-hidden h-96 grid place-content-center p-2">
+          <h3 class="text-3xl font-semibold text-gray-100 z-10 relative">Calidad de la Semilla</h3>
+          <img
+            src="@/assets/images/semilla.jpg"
+            alt=""
+            class="absolute inset-0 z-0 brightness-50 h-full object-cover w-full"
+          />
+        </UCard>
+      </section>
+
+      <section class="flex justify-center mt-8">
+        <UButton to="/" color="green" size="lg" icon="i-heroicons-arrow-small-right"
+          >Conoce más sobre nuestros servicios</UButton
+        >
+      </section>
+    </section>
+
+    <section>
+      <h2 class="text-5xl font-semibold text-primary text-center">Certificaciones</h2>
+      <p class="text-xl text-center mt-4">Algunas de nuestras certificaciones</p>
+    </section>
+  </UContainer>
+</template>
